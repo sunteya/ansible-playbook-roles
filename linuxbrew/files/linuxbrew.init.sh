47ee3e69dca64117e2adedb8b3ee2b302f082550
sunteya <sunteya@gmail.com>
2014-08-14 14:04:08 +0800

    linuxbrew: fix brew pkg-config version don’t use system pkg-config config

--- linuxbrew/files/linuxbrew.init.sh
+++ linuxbrew/files/linuxbrew.init.sh
@@ -1,6 +1,10 @@
 export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 export HOMEBREW_CACHE=/var/cache/Homebrew
 
+if [ -x /usr/bin/pkg-config ]; then
+	export PKG_CONFIG_LIBDIR=$(/usr/bin/pkg-config --variable pc_path pkg-config)
+fi
+
 if [ -d $(brew --prefix)/etc/bash_completion.d ]; then
 	for f in $(brew --prefix)/etc/bash_completion.d/*; do
 		source $f;
