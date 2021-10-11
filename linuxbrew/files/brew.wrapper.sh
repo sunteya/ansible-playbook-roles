ba1cc5c5c7314a7375ae8e425057ef8dca3f931f
sunteya <sunteya@gmail.com>
2021-10-11 13:06:08 +0800

    linuxbrew: remove linuxbrew-wrapper

---
+++ linuxbrew/files/brew.wrapper.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+BREW_PREFIX=/home/linuxbrew/.linuxbrew
+exec $BREW_PREFIX/bin/brew "$@"
--- linuxbrew/files/linuxbrew.init.sh
+++ linuxbrew/files/linuxbrew.init.sh
@@ -1,11 +1,14 @@
-export HOMEBREW_CACHE=/var/cache/Homebrew
+# export HOMEBREW_CACHE=/var/cache/Homebrew
 
-if [ -x /usr/bin/pkg-config ]; then
-	export PKG_CONFIG_LIBDIR=$(/usr/bin/pkg-config --variable pc_path pkg-config)
-fi
+# if [ -x /usr/bin/pkg-config ]; then
+# 	export PKG_CONFIG_LIBDIR=$(/usr/bin/pkg-config --variable pc_path pkg-config)
+# fi
+#
+# if [ -d $(brew --prefix)/etc/bash_completion.d ]; then
+# 	for f in $(brew --prefix)/etc/bash_completion.d/*; do
+# 		source $f;
+# 	done
+# fi
 
-if [ -d $(brew --prefix)/etc/bash_completion.d ]; then
-	for f in $(brew --prefix)/etc/bash_completion.d/*; do
-		source $f;
-	done
-fi
+BREW_PREFIX=/home/linuxbrew/.linuxbrew
+export PATH=$BREW_PREFIX/bin:$PATH
